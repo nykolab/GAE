f3430d38de42f1aff045f8d989bf2f2e997b4d91
Nykolab <nykolab@gmail.com>
2015-06-26 00:25:32 +0300

    added user services

--- GAE/src/helloworld.py
+++ GAE/src/helloworld.py
@@ -1,10 +1,20 @@
+from google.appengine.api import users
+
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-
+        
+        # Checks for active Google account session
+        user = users.get_current_user()
+        
+        if user:
+            self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+            self.response.write('Hello, ' + user.nickname() + 
+                                ' Email: ' + user.email())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+        
 routes = [('/', MainPage),]
 
 app = webapp2.WSGIApplication(routes, debug=True)
